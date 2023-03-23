96458a5de08420f6bb6d1cf619946b5faf37b2ed
JoharAbbas-313 <123437515+JoharAbbas-313@users.noreply.github.com>
2023-03-23 11:41:11 -0700

    Add files via upload

---
+++ SMIT JAWASCRIPT/app.js
@@ -0,0 +1,82 @@
+// var d = new Date();
+//  var mydate = d.getDate();
+//  document.write(mydate);
+
+// getFullYear Function
+
+// var d = new Date();
+//  var myyear = d.getFullYear();
+//  document.write(myyear);
+
+// getMONTH FUNCTION 
+
+// var d = new Date();
+//  var mymonth = d.getMonth();
+//  document.write(mymonth);
+
+// IS FUNCTION KO ISTEMAL KR K CURRENT DATE AUR DIN DONONG HASAAL KR SAKTE HE
+
+// toDateString Function
+
+// var d = new Date();
+//  var mydate = d.toDateString();
+//  document.write(mydate);
+
+
+// IS FUNCTION KI MUDAD SA HUM MUKAMAL DIN AUR MAHEENA LEAKH SAKTE HE
+
+// toLocaleDateString Function
+
+// var d = new Date();
+//  var mydate = d.toLocaleDateString();
+//  document.write(mydate);
+
+
+// JAWASCRIPT MA IS FUNCTION KI MUDAD SA HUM CURRENT TIME HASAAL KR SAKTE HE    
+
+// toLocaleTimeString Function
+
+// var d = new Date();
+//  var mytime = d.toLocaleTimeString();
+//  document.write(mytime);
+
+
+// IS FUNCTION KI MUDAD SA HUM DATE KO CHANGE KR K LEAKH SAKTE HE
+
+// setDATE FUNCTION 
+
+// var dt = new Date("Aug 5 2013");
+//  dt.setDate(9);
+//  document.write(dt);
+
+// IS FUNCTION KI MUDAD SA HUM YEAR KO BI CHANGE KR SAKTE HE
+
+// setFullYear Function
+
+// var d = new Date("Aug 5 2013")
+//  d.setFullYear(d.getFullYear() + 1)
+//  document.write(d)
+
+// IS FUNCTION KO ISTEMAAL KRTA HUWA HUM TIME KO BI CHANGE KR SAKTE HE
+
+// setHours Function
+
+// var dt = new Date("Aug 28, 2010 03:00:00");
+//  dt.setHours(5);
+//  document.write(dt);
+
+// IS FUNCTION KO ISTEEMAK KR K HUM MINUTES KO BI CHANGE KR SAKTE HE
+
+// setMinutes Function
+
+// var dt = new Date("Aug 28, 2010 03:27:00");
+//  dt.setMinutes(45);
+//  document.write(dt);
+
+// IS FUNCTION SA HUM MONTH KO BI TABDEEL KR SAKTE HE 
+
+// setMONTH FUNCTION
+
+// var dt = new Date("Aug 28, 2010");
+//  dt.setMonth(8);
+//  document.write(dt);
